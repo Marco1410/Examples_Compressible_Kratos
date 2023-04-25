b2cdbc104f35cfe63d7682b0e8ae74280afdea67
marco1410 <marcozuniga1410@mail.com>
2023-04-25 07:29:51 +0200

    update

--- Compressible Potential Flow/PotentialFlow_Ale_example/MainKratos.py
+++
@@ -1,9 +0,0 @@
-import KratosMultiphysics
-from KratosMultiphysics.CompressiblePotentialFlowApplication.potential_flow_analysis import PotentialFlowAnalysis
-
-with open("ProjectParametersPrimal.json",'r') as parameter_file:
-	parameters = KratosMultiphysics.Parameters(parameter_file.read())
-
-model = KratosMultiphysics.Model()
-simulation = PotentialFlowAnalysis(model,parameters)
-simulation.Run()
