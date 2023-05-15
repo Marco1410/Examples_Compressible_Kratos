5607742e474079a78956e739b3a12890c6290fc9
marco1410 <marcozuniga1410@gmail.com>
2023-05-15 17:46:39 +0200

    update

--- transonic/MainKratos.py
+++ transonic/MainKratos.py
@@ -55,14 +55,13 @@ def load_mu_parameters():
     return mu
 
 def M_cr(cp_min,critical_mach):
+    #Karman-Tsien rule (mejor que Prandtl-Glauert que no es preciso para M > 0.7)
     return cp_min/np.sqrt(1-critical_mach**2)-((1/(0.7*critical_mach**2))*((((2+0.4*critical_mach**2)/2.4)**3.5)-1))
 
 def shock_parameters(mach_infinity,angle_of_attack):
-    # if mach_infinity < 0.7:
+    # -0.6 cp min del naca 0012 a 0º
+    # debe ser el menor cp del ala/perfil en regimen incompresible
     cp_min = -0.6
-    #     critical_cp = -0.6 / np.sqrt(1-mach_infinity**2)
-    # else:  
-    #     critical_cp = -0.6 / (np.sqrt(1-mach_infinity**2)-(0.6*(mach_infinity**2/np.sqrt(1-mach_infinity**2))/2)) # -0.6 cp min a cero grados del naca0012
     # Bisection method
     a = 0.2
     b = 0.95
@@ -70,11 +69,13 @@ def shock_parameters(mach_infinity,angle_of_attack):
     critical_mach = (a + b) / 2.0
     while True:
         if b - a < tol:
-            critical_mach = 0.6
-            upwind_factor_constant = 2
-            print(mach_infinity,critical_mach)
+            #critical_mach = 0.6
+            mach_number_limit = critical_mach * 2.0
+            upwind_factor_constant = mach_number_limit**2 * 1.6
+            print(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+            print(mach_infinity,critical_mach,mach_number_limit,upwind_factor_constant)
             #input()
-            return critical_mach,upwind_factor_constant
+            return critical_mach,upwind_factor_constant,mach_number_limit
         elif M_cr(cp_min,a) * M_cr(cp_min,critical_mach) > 0:
             a = critical_mach
         else:
@@ -89,68 +90,71 @@ if __name__ == "__main__":
     ################################
     ################################
     ################################
-    mach_range  = [0.67,0.88]
-    angle_range = [1.99,2.01]
-    number_of_point_test = 5
+    # en general el mach de crucero (en transonico) < 0.9
+    # y el angulo de ataque < 5º
+    mach_range  = [0.65,0.75]
+    angle_range = [2.0,4.5]
+    number_of_point_test = 15
     ################################
     ################################
     ################################
 
-    mu_test  = get_multiple_params_by_Halton(number_of_point_test,angle_range,mach_range)
-    save_mu_parameters(mu_test)
-    plot_mu_values(mu_test,"Mu_Values.png",mach_range[0],mach_range[1],angle_range[0],angle_range[1])
-    #mu_test = load_mu_parameters()
+    #mu_test  = get_multiple_params_by_Halton(number_of_point_test,angle_range,mach_range)
+    #save_mu_parameters(mu_test)
+    #plot_mu_values(mu_test,"Mu_Values.png",mach_range[0],mach_range[1],angle_range[0],angle_range[1])
+    mu_test = load_mu_parameters()
 
     mesh_list = ["naca0012_0aoa"]
 
-    #plot cp en 3D
-    cp3d = plt.figure()
-    cp3d.subplots_adjust()
-    cp3d.set_figwidth(20.0)
-    cp3d.set_figheight(15.0)
-    # Creamos el plano 3D
-    ax3 = cp3d.add_subplot(111, projection='3d')
-    ax3.set_xlabel('x')
-    ax3.set_ylabel('y')
-    ax3.set_zlabel('Cp')    
-    ax3.set_title('Cp vs x - naca0012')
-    ax3.invert_zaxis()
-    ax3.grid()
+    # #plot cp en 3D
+    # cp3d = plt.figure()
+    # cp3d.subplots_adjust()
+    # cp3d.set_figwidth(15.0)
+    # cp3d.set_figheight(10.0)
+    # # Creamos el plano 3D
+    # ax3 = cp3d.add_subplot(111, projection='3d')
+    # ax3.set_xlabel('x')
+    # ax3.set_ylabel('y')
+    # ax3.set_zlabel('Cp')    
+    # ax3.set_title('Cp vs x - naca0012')
+    # ax3.invert_zaxis()
+    # ax3.grid()
 
-    # # airfoils vs cp vs x
-    # fig  = plt.figure()
-    # fig.subplots_adjust(top=0.8)
-    # fig.set_figwidth(20.0)
-    # fig.set_figheight(15.0)
+    # airfoils vs cp vs x
+    fig  = plt.figure()
+    fig.subplots_adjust(top=0.8)
+    fig.set_figwidth(20.0)
+    fig.set_figheight(15.0)
 
-    # ax1 = fig.add_subplot(211)
-    # ax1.axis([-0.6,0.65,-3.0,1.5])
-    # ax1.set_ylabel('Cp')
-    # ax1.set_title('Cp vs x')
-    # ax1.invert_yaxis()
-    # ax1.grid()
+    ax1 = fig.add_subplot(211)
+    ax1.axis([-0.6,0.65,-3.0,1.5])
+    ax1.set_ylabel('Cp')
+    ax1.set_title('Cp vs x')
+    ax1.invert_yaxis()
+    ax1.grid()
 
-    # ax2 = fig.add_subplot(212)
-    # ax2.axis([-0.6,0.65,-0.2,0.2])
-    # ax2.set_ylabel('y')
-    # ax2.set_xlabel('x')
-    # ax2.set_title('Airfoils')
-    # ax2.grid()
+    ax2 = fig.add_subplot(212)
+    ax2.axis([-0.6,0.65,-0.2,0.2])
+    ax2.set_ylabel('y')
+    ax2.set_xlabel('x')
+    ax2.set_title('Airfoils')
+    ax2.grid()
 
     for mesh_name in mesh_list:
         parameters["solver_settings"]["model_import_settings"]["input_filename"].SetString(mesh_name)
-        # fout=open("critical_cp_mach_"+mesh_name+".dat", 'w')
-        # fout.write("# Angle_of_atack mach_infinity Critical_Cp Critical_Mach upwind_factor_constant \n")
+        fout=open("critical_cp_mach_"+mesh_name+".dat", 'w')
+        fout.write("# Angle_of_atack mach_infinity Critical_Cp Critical_Mach upwind_factor_constant \n")
         for id, mu in enumerate(mu_test):
             angle_of_attack = mu[1]
             mach_infinity = mu[0]
 
-            critical_mach,upwind_factor_constant = shock_parameters(mach_infinity,angle_of_attack)
+            critical_mach,upwind_factor_constant,mach_number_limit = shock_parameters(mach_infinity,angle_of_attack)
 
             parameters["processes"]["boundary_conditions_process_list"][0]["Parameters"]["angle_of_attack"].SetDouble(angle_of_attack)
             parameters["processes"]["boundary_conditions_process_list"][0]["Parameters"]["mach_infinity"].SetDouble(mach_infinity)
             parameters["processes"]["boundary_conditions_process_list"][0]["Parameters"]["critical_mach"].SetDouble(critical_mach)
             parameters["processes"]["boundary_conditions_process_list"][0]["Parameters"]["upwind_factor_constant"].SetDouble(upwind_factor_constant)
+            parameters["processes"]["boundary_conditions_process_list"][0]["Parameters"]["mach_number_limit"].SetDouble(mach_number_limit)
 
             model = KratosMultiphysics.Model()
             simulation = PotentialFlowAnalysis(model,parameters)
@@ -164,17 +168,17 @@ if __name__ == "__main__":
             for i,node in enumerate(modelpart.Nodes):
                 x[i] = node.X0 ; y[i] = node.Y0 ; z[i] = node.Z0
                 cp[i] = node.GetValue(KratosMultiphysics.PRESSURE_COEFFICIENT)
-            # ax1.plot( x, cp, "o", markersize = 3.0, label = mesh_name + "_" + str(np.round(mu[0] * 180 / np.pi,1))+"º_" + str(np.round(mu[1],2)))
-            ax3.scatter(x+0.5,id*10,cp,".",label=str(np.round(mu[1]*180/np.pi,1))+"º_"+str(np.round(mu[0],2)))
-        # fout.close()
-        # ax2.plot( x, y, "o", markersize = 3.0, label = mesh_name)
+            ax1.plot( x, cp, "o", markersize = 3.0, label = mesh_name + "_" + str(np.round(mu[1] * 180 / np.pi,1))+"º_" + str(np.round(mu[0],2)))
+            # ax3.scatter(x+0.5,id*10,cp,".",label=str(np.round(mu[1]*180/np.pi,1))+"º_"+str(np.round(mu[0],2)))
+        fout.close()
+        ax2.plot( x, y, "o", markersize = 3.0, label = mesh_name)
 
-    # ax1.legend()
-    # ax2.legend()
-    # fig = plt.tight_layout()
-    # fig = plt.savefig("Airfoils_Cp_x.png")
-    ax3.legend()
-    cp3d = plt.tight_layout()
-    cp3d = plt.savefig("cp_3d")
+    ax1.legend()
+    ax2.legend()
+    fig = plt.tight_layout()
+    fig = plt.savefig("Airfoils_Cp_x.png")
+    # ax3.legend()
+    # cp3d = plt.tight_layout()
+    # cp3d = plt.savefig("cp_3d")
     
 
